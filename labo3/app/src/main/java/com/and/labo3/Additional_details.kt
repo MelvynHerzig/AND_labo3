2de9a00a39998f98f05d6953a19256a8164ab140
Quentin Forestier <quentin.forestier@heig-vd.ch>
2022-03-22 21:08:16 +0100

    Layout Quentin

---
+++ labo3/app/src/main/java/com/and/labo3/Additional_details.kt
@@ -0,0 +1,11 @@
+package com.and.labo3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Additional_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_additional_details)
+    }
+}
