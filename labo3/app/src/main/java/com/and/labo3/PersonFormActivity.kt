7de4042e59fa179aa98f347157345c7a26bd63aa
Herzig Melvyn <melvyn.herzig@heig-vd.ch>
2022-03-22 20:18:22 +0100

    First part of form

--- labo3/app/src/main/java/com/and/labo3/PersonFormActivity.kt
+++ labo3/app/src/main/java/com/and/labo3/PersonFormActivity.kt
@@ -3,9 +3,9 @@ package com.and.labo3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PersonFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_person_form)
     }
 }
